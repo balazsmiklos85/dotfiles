64ae08167227276fef7531822020a5b6e4507cac
Miklós Balázs <balazsmiklos85+git@gmail.com>
2023-12-09 16:34:43 +0100

    Add paths to PATH variable in fish config
    This way the `$HOME` variable, which is different on MacOS and Linux is resolved when the directories are added.

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,11 @@
 set theme_color_scheme zenburn
 
+set -gx PATH $HOME/.local/bin $PATH
+set -gx PATH $HOME/.nvm $PATH
+set -gx PATH $HOME/.sdkman $PATH
+set -gx PATH $HOME/projects/scripts $PATH
+set -gx PATH $HOME/projects/hybris/core-customize/hybris/bin/platform/apache-ant/bin $PATH
+
 if command -v thefuck > /dev/null
     thefuck --alias | source
 end
@@ -10,5 +16,7 @@ end
 
 if [ (uname) = "Darwin" ]
     # Commands to run only on MacOS can go here
-    set -x theme_powerline_fonts no
+    set -x theme_powerline_fonts no    
+    set -gx PATH /opt/homebrew/bin $PATH
+    set -gx PATH /opt/homebrew/opt/ruby/bin $PATH
 end
