04246690545006c5dfc0b7ebb46de354e0265651
Miklos Balazs <balazsmiklos85@gmail.com>
2024-01-10 16:19:54 +0100

    🔧 Java specific settings

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,3 +1,7 @@
+vim.cmd 'set shiftwidth=4 smarttab'
+vim.cmd 'set expandtab'
+vim.cmd 'set cc=120'
+
 local config = {
     cmd = {os.getenv("HOME") .. '/.local/bin/jdtls/bin/jdtls'},
     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
