a644444a2f3ce0ba1349ba43c3cbdd0da71351cf
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-01-21 12:34:46 +0100

    🔧 Fixes the personal scripts path.

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ source ~/.config/fish/secrets.fish
 set theme_color_scheme zenburn
 
 set -gx PATH $HOME/.local/bin $PATH
-set -gx PATH $HOME/.local/scripts $PATH
+set -gx PATH $HOME/.local/bin/scripts $PATH
 set -gx PATH $HOME/.nvm $PATH
 set -gx PATH $HOME/.sdkman $PATH
 set -gx PATH $HOME/projects/hybris/core-customize/hybris/bin/platform/apache-ant/bin $PATH
