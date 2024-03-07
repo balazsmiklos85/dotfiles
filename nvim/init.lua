28eef637bfed51014f80e9d13fdb52240e1c5c1f
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-03-07 15:21:34 +0000

    🔧 color scheme is set correctly: Linux can use true color.

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,10 +5,11 @@ require('lsp')
 vim.keymap.set('n', '<C-p>', ':Files<CR>')
 
 vim.cmd[[set background=dark]]
-if vim.fn.match(vim.env.TERM, 'xterm') >= 0 then
-    vim.cmd[[colorscheme palenight]]
-else
+
+if io.popen("uname"):read("*l") == 'Linux' and not (os.getenv("SSH_CLIENT") or os.getenv("SSH_TTY")) then
     vim.cmd[[colorscheme catppuccin]]
+else
+    vim.cmd[[colorscheme palenight]]
 end
 
 vim.cmd[[set number]]
