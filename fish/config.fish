d3d4f5460e8a60b484a4eda083f7e13632e42426
Miklos Balazs <balazsmiklos85@gmail.com>
2024-03-11 12:10:21 +0100

    🔧 Unnecessary conditions/comments removed.

--- fish/config.fish
+++ fish/config.fish
@@ -8,7 +8,6 @@ set -gx PATH $HOME/.nvm $PATH
 set -gx PATH $HOME/.sdkman $PATH
 
 if [ (uname) = "Darwin" ]
-    # Commands to run only on MacOS can go here
     set -gx PATH /opt/homebrew/bin $PATH
     set -gx PATH /opt/homebrew/opt/ruby/bin $PATH
     set -gx PATH $HOME/DEV/projects/hybris/core-customize/hybris/bin/platform/apache-ant/bin $PATH
@@ -18,9 +17,5 @@ end
 
 if command -v thefuck > /dev/null
     thefuck --alias | source
-end
-
-if status is-interactive
-    # Commands to run in interactive sessions can go here
 end
 
