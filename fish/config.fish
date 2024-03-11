7c72e2efded27251dcf009b8caefea9e5343017c
Miklos Balazs <balazsmiklos85@gmail.com>
2024-03-11 12:09:38 +0100

    🔧 Work project path changed.

--- fish/config.fish
+++ fish/config.fish
@@ -6,12 +6,12 @@ set -gx PATH $HOME/.local/bin $PATH
 set -gx PATH $HOME/.local/bin/scripts $PATH
 set -gx PATH $HOME/.nvm $PATH
 set -gx PATH $HOME/.sdkman $PATH
-set -gx PATH $HOME/projects/hybris/core-customize/hybris/bin/platform/apache-ant/bin $PATH
 
 if [ (uname) = "Darwin" ]
     # Commands to run only on MacOS can go here
     set -gx PATH /opt/homebrew/bin $PATH
     set -gx PATH /opt/homebrew/opt/ruby/bin $PATH
+    set -gx PATH $HOME/DEV/projects/hybris/core-customize/hybris/bin/platform/apache-ant/bin $PATH
 else
     set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
 end
