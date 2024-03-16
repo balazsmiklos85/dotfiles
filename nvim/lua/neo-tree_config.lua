d7f9178fa15efb874a6d96e5a5a4c188936ebe9e
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-03-16 15:09:13 +0000

    🔧 neo-tree warning fixed.

--- nvim/lua/neo-tree_config.lua
+++ nvim/lua/neo-tree_config.lua
@@ -3,7 +3,9 @@ require('neo-tree').setup {
       close_if_last_window = true,
       auto_clean_after_session_restore = true,
       filesystem = {
-        follow_current_file = true,
+        follow_current_file = {
+          enabled = true,
+        },
         hijack_netrw_behavior = "open_current",
         use_libuv_file_watcher = true,
       },
