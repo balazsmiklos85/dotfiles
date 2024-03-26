07ef31284c7d4f37e4ad4eda95c038b8040daa4a
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-03-26 07:07:34 +0100

    🔧 Obsidian workspace is loaded dynamically whether it exists or not.

--- nvim/lua/obsidian_config.lua
+++ nvim/lua/obsidian_config.lua
@@ -1,9 +1,17 @@
-require('obsidian').setup {
-  workspaces = {
-    {
-      name = "notes",
-      path = "~/Documents/notes",
-    },
-  },
+local Path = require("plenary.path")
+
+local workspaces = {}
+
+local documents_en = os.getenv("HOME") .. "/Documents/notes"
+local documents_hu = os.getenv("HOME") .. "/Dokumentumok/notes"
+
+if Path:new(documents_en):exists() then
+  table.insert(workspaces, { path = documents_en, name = "notes" })
+elseif Path:new(documents_hu):exists() then
+  table.insert(workspaces, { path = documents_hu, name = "notes" })
+end
+
+require("obsidian").setup {
+  workspaces = workspaces,
 }
 
