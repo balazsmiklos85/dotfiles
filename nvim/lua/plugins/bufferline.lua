bc4a7e37e294fa867a7430a39005d55b7e895b26
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-04-08 19:03:15 +0200

    🔧 `bufferline.nvim` config fixed.

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -4,27 +4,29 @@ return {
   dependencies = {
     'nvim-tree/nvim-web-devicons'
   },
-  options = {
-    offsets = {
-      {
-        filetype = "neo-tree",
-        text = "Folder",
-        text_align = "left",
-        separator = "│",
+  opts = {
+    options = {
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "Folder",
+          text_align = "left",
+          separator = "│",
+        },
+        {
+          filetype = "dapui_watches",
+          text = "Debugger",
+          text_align = "left",
+          separator = "│",
+        },
+        {
+          filetype = "dbui",
+          text = "Database",
+          text_align = "left",
+          separator = "│",
+        },
       },
-      {
-        filetype = "dapui_watches",
-        text = "Debugger",
-        text_align = "left",
-        separator = "│",
-      },
-      {
-        filetype = "dbui",
-        text = "Database",
-        text_align = "left",
-        separator = "│",
-      },
-    },
+    }
   }
 }
 
