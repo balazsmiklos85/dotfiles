fe54611c2652954d51e74e2d1a2c66b6fd9a8185
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-04-22 17:11:45 +0200

    🔧 using `ruby_lsp` as `ruby_ls` is deprecated.

--- nvim/ftplugin/ruby.lua
+++ nvim/ftplugin/ruby.lua
@@ -2,6 +2,6 @@ vim.cmd 'setlocal shiftwidth=2 smarttab'
 vim.cmd 'setlocal expandtab'
 
 local lspconfig = require('lspconfig')
-lspconfig.ruby_ls.setup{}
+lspconfig.ruby_lsp.setup{}
 lspconfig.solargraph.setup{}
 
