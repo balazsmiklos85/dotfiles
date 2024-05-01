7570858c7a6dc9d023e2c5cefad5338548fcde75
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-05-01 08:53:21 +0200

    🔧 limiting `sonarlint.nvim` to Java files. Still does not work.

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -7,3 +7,19 @@ local config = {
 }
 require('jdtls').start_or_attach(config)
 
+require('sonarlint').setup({
+   server = {
+      cmd = {
+         'sonarlint-language-server',
+         '-stdio',
+         '-analyzers',
+         vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarpython.jar"),
+         vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarcfamily.jar"),
+         vim.fn.expand("$MASON/share/sonarlint-analyzers/sonarjava.jar"),
+      }
+   },
+   filetypes = {
+      'java',
+   }
+})
+
