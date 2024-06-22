b8787ab4d405eda15775147cba9ac1661ac4734e
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-06-22 13:30:08 +0200

    🧑‍💻 installing everything I need for openSUSE.

---
+++ fish/functions/init_opensuse.fish
@@ -0,0 +1,6 @@
+#!/usr/bin/fish
+
+function init_opensuse
+	sudo zypper install --no-recommends fd neomutt password-store evince-plugin-djvudocument powertop youtube-dl graphviz w3m lynx lsd git-delta ripgrep bat telnet encfs symbols-only-nerd-fonts powerline-fonts thefuck neovim mc git docker docker-compose fish syncthing
+end
+
