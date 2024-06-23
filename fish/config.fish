ea251193130cad125a0a8f2e613a89ac81157eb9
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-06-23 11:04:01 +0200

    🔧 `PATH` optimized.

--- fish/config.fish
+++ fish/config.fish
@@ -2,26 +2,21 @@ source ~/.config/fish/secrets.fish
 
 set -g theme_color_scheme zenburn
 
-set -gx PATH $HOME/.local/bin $PATH
-set -gx PATH $HOME/.local/bin/scripts $PATH
-set -gx PATH $HOME/.nvm $PATH
 set -gx PATH $HOME/.sdkman $PATH
 
 if [ (uname) = "Darwin" ]
-    set -gx PATH /opt/homebrew/bin $PATH
-    set -gx PATH /opt/homebrew/opt/ruby/bin $PATH
-    set -gx PATH $HOME/DEV/projects/hybris/core-customize/hybris/bin/platform/apache-ant/bin $PATH
-    if am_i_at_work
-        set -g theme_color_scheme light
-    end
-    set -gx GIT_EDITOR /opt/homebrew/bin/nvim
-else
-    set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
+	set -gx PATH /opt/homebrew/bin $PATH
+	set -gx PATH $HOME/DEV/projects/hybris/core-customize/hybris/bin/platform/apache-ant/bin $PATH
+	set -gx PATH $HOME/.nvm $PATH
+	if am_i_at_work
+		set -g theme_color_scheme light
+	end
+	set -gx GIT_EDITOR /opt/homebrew/bin/nvim
 end
 
 set -gx MAILCAPS $HOME/.config/mailcap
 
 if command -v thefuck > /dev/null
-    thefuck --alias | source
+	thefuck --alias | source
 end
 
