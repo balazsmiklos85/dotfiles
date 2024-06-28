b32255baa423f7867196c079fa4c2b76c1bde097
Balazs Miklos <balazsmiklos85@gmail.com>
2024-06-28 07:38:00 +0200

    🔧 switching to light theme includes `kitty` and `bat`.

--- fish/functions/switch_light.fish
+++ fish/functions/switch_light.fish
@@ -4,8 +4,12 @@ function switch_light
     if test $status -eq 0
         osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to false'
         set -g theme_color_scheme light
+	set -gx BAT_THEME "Catppuccin Latte"
+	kitty +kitten themes --reload-in=all Catppuccin-Latte
     else if [ (uname) = "Darwin" ]
         osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to true'
         set -g theme_color_scheme zenburn
+	set -gx BAT_THEME "Catppuccin Mocha"
+	kitty +kitten themes --reload-in=all Catppuccin-Mocha
     end
 end
