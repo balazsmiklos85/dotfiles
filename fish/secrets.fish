36ed2bbd435a188d875b4b5034f07bd985c6839f
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-06-29 09:22:26 +0200

    🔐 Not storing raw secrets in the file system, using `pass` instead.

--- fish/secrets.fish
+++ fish/secrets.fish
@@ -1,10 +1,4 @@
 #!/usr/bin/env fish
 
-set directory ~/.config/fish/secrets
+set -xg SONARCLOUD_TOKEN (pass show token/sonarcloud)
 
-for file in (find $directory -name "*.token")
-    set filename (basename $file .token)
-    set varname (echo $filename | tr '[:lower:]' '[:upper:]')_TOKEN
-    set content (cat $file)
-    set -xg $varname $content
-end
