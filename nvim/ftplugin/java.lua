1cd55955ebb01e71ffc5d0cb53d6fd210576ae8b
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-06-30 18:50:52 +0200

    🔧 Using the `mason` version of `jdtls`.

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -2,7 +2,7 @@ vim.cmd 'setlocal shiftwidth=4 smarttab'
 vim.cmd 'setlocal expandtab'
 
 local config = {
-    cmd = {os.getenv("HOME") .. '/.local/bin/jdtls/bin/jdtls'},
+    cmd = {os.getenv("HOME") .. '/.local/share/nvim/mason/packages/jdtls/bin/jdtls'},
     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw'}, { upward = true })[1]),
 }
 require('jdtls').start_or_attach(config)
