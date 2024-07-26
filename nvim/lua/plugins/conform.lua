ae75b506f371d65ab1f4e3b321d30c248a8540ae
Balazs Miklos <balazsmiklos85@gmail.com>
2024-07-26 10:42:13 +0200

    🔧 key binding for code formatting.

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,5 +1,15 @@
 return {
 	"stevearc/conform.nvim",
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format()
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
 	opts = {
 		formatters_by_ft = {
 			java = { "clang-format" },
