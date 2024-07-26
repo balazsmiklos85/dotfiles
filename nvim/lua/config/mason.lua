0ad2a89d61605c4ce4046c47540036a8e983c5f7
Balazs Miklos <balazsmiklos85@gmail.com>
2024-07-26 15:42:59 +0200

    ➕ adding Kotlin support.

--- nvim/lua/config/mason.lua
+++ nvim/lua/config/mason.lua
@@ -1,27 +1,34 @@
-require('mason').setup()
-require('mason-lspconfig').setup {
-  ensure_installed = {
-    -- Java
-    "jdtls",
-    -- Lua
-    "lua_ls",
-    -- Ruby
-    "rubocop",
-    "ruby_lsp",
-    "sorbet",
-    "solargraph",
-    -- Rust
-    "rust_analyzer",
-  }
+local lsps_to_install = {
+	-- Java
+	"jdtls",
+	-- Lua
+	"lua_ls",
 }
-require('mason-tool-installer').setup {
-  ensure_installed = {
-    -- Java
-    'clang-format',
-    "java-debug-adapter",
-    "java-test",
-    -- Lua
-    'stylua',
-  }
+local tools_to_install = {
+	-- Java
+	"clang-format",
+	"java-debug-adapter",
+	"java-test",
+	-- Lua
+	"stylua",
 }
 
+local is_macos = io.popen("uname"):read("*l") == "Darwin"
+if is_macos then
+	table.insert(lsps_to_install, "kotlin_language_server")
+else
+	-- Ruby
+	table.insert(lsps_to_install, "rubocop")
+	table.insert(lsps_to_install, "ruby_lsp")
+	table.insert(lsps_to_install, "sorbet")
+	table.insert(lsps_to_install, "solargraph")
+	-- Rust
+	table.insert(lsps_to_install, "rust_analyzer")
+end
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = lsps_to_install,
+})
+require("mason-tool-installer").setup({
+	ensure_installed = tools_to_install,
+})
