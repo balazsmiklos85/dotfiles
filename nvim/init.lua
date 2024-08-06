0e1036c83263b96571eac8725c0fedc6dd725b2b
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-08-06 17:02:32 +0200

    🚚 moved the theme choosing logic to the theme plugin.

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,23 +9,7 @@ require("config.lspconfig")
 require("config.dap-ui")
 require("config.telescope")
 
-if util.is_ssh() then
-	vim.cmd([[colorscheme catppuccin-frappe]])
-else
-	local wifi = ""
-	if util.is_macos() then
-		local handle = io.popen("networksetup -getairportnetwork en0")
-		wifi = handle:read("*a")
-		handle:close()
-	end
-
-	if string.match(wifi, "FN%-BYOD") then
-		vim.cmd([[colorscheme catppuccin-latte]])
-	else
-		vim.cmd([[colorscheme catppuccin]])
-	end
-end
-
+vim.cmd([[colorscheme catppuccin]])
 vim.cmd([[set linebreak]])
 vim.cmd([[set conceallevel=1]])
 vim.cmd([[set number]])
--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -1,7 +1,24 @@
+local util = require("util.functions")
+local flavour = "auto" -- latte, frappe, macchiato, mocha
+if util.is_ssh() then
+	flavour = "frappe"
+else
+	local wifi = ""
+	if util.is_macos() then
+		local handle = io.popen("networksetup -getairportnetwork en0")
+		wifi = handle:read("*a")
+		handle:close()
+	end
+	if string.match(wifi, "FN%-BYOD") then
+		flavour = "latte"
+	end
+end
+
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	opts = {
+		flavour = flavour,
 		transparent_background = true,
 
 		integrations = {
