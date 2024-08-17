96d268af3ffbc3a954e5409a7e2e67a893560725
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-08-17 19:35:04 +0200

    🚚 UI related settings moved forward.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,14 @@
 vim.keymap.set("n", " ", "<Nop>", { silent = true, remap = false })
 vim.g.mapleader = " "
 
+vim.cmd([[set linebreak]])
+vim.cmd([[set conceallevel=1]])
+vim.cmd([[set number]])
+vim.cmd([[set list]])
+
 require("config.lazy")
 require("config.mason")
 require("config.keymaps")
 
 vim.cmd([[colorscheme catppuccin]])
-vim.cmd([[set linebreak]])
-vim.cmd([[set conceallevel=1]])
-vim.cmd([[set number]])
-vim.cmd([[set list]])
+
