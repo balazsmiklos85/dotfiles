106d7e8e1e43650cddbea4ac6abbcd07eef1edb1
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-08-19 07:12:13 +0200

    🔧 Removing key bindings from the Obsidian plugin as they prevent the plugin loading.

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -13,22 +13,6 @@ return {
 		"nvim-lua/plenary.nvim",
 		"hrsh7th/nvim-cmp",
 	},
-	keys = {
-		{
-			"<leader>on",
-			":ObsidianNew<CR>",
-			mode = "n",
-
-			desc = "New Obsidian note",
-		},
-		{
-			"<leader>od",
-			":ObsidianDailies<CR>",
-			mode = "n",
-
-			desc = "New Obsidian daily note",
-		},
-	},
 	opts = {
 		workspaces = {
 			{
