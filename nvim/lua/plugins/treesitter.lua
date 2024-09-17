6ac09cc27668f0fb85d59b4712f5c9f20a439cba
Balazs Miklos <balazsmiklos85@gmail.com>
2024-09-17 15:04:30 +0200

    🔧 trying to add CSV formatting to Treesitter.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,35 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  opts = {
-    ensure_installed = {
-      "dockerfile",
-      "fish",
-      "gitcommit",
-      "gitignore",
-      "groovy",
-      "html",
-      "java",
-      "javascript",
-      "lua",
-      "ruby",
-      "rust",
-      "vim",
-      "xml",
-    },
-    sync_install = false,
-    auto_install = false,
+	"nvim-treesitter/nvim-treesitter",
+	opts = {
+		ensure_installed = {
+			"csv",
+			"dockerfile",
+			"fish",
+			"gitcommit",
+			"gitignore",
+			"groovy",
+			"html",
+			"java",
+			"javascript",
+			"lua",
+			"ruby",
+			"rust",
+			"vim",
+			"xml",
+		},
+		sync_install = false,
+		auto_install = false,
 
-    highlight = {
-      enable = true,
+		highlight = {
+			enable = true,
 
-      disable = function(lang, buf)
-        local max_filesize = 200 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-            return true
-        end
-      end,
-    },
-  }
+			disable = function(lang, buf)
+				local max_filesize = 200 * 1024 -- 100 KB
+				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+				if ok and stats and stats.size > max_filesize then
+					return true
+				end
+			end,
+		},
+	},
 }
-
