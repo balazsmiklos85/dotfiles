9fd7b52b3a3ac279a82cb852633850f30540ad7b
Balazs Miklos <balazsmiklos85@gmail.com>
2024-09-20 07:34:43 +0200

    🔧 detecting the home network range instead.

--- fish/functions/am_i_at_work.fish
+++ fish/functions/am_i_at_work.fish
@@ -1,8 +1,8 @@
 
 function am_i_at_work
     if [ (uname) = "Darwin" ]
-        set ip_third_octet (ifconfig | rg 'inet\s' | rg '192.168' | awk '{print $2}' | awk -F. '{print $3}')
-        if [ "$ip_third_octet" != "0" ]
+        set home_ip (ifconfig | rg 'inet\s' | rg '192.168.0')
+        if [ "$home_ip" = "" ]
             return 0
         else
             return 1
