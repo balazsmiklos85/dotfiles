49b6f19ac3b872f75b293c411bfc6a22b353df3d
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-09-23 20:04:05 +0200

    🔧 using Catppuccin with fish.

--- fish/functions/switch_light.fish
+++ fish/functions/switch_light.fish
@@ -3,13 +3,13 @@ function switch_light
 
 	if test $status -eq 0
 		osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to false'
-		set -g theme_color_scheme switch_light
+		set -g theme_color_scheme "Catppuccin Latte"
 		set -gx BAT_THEME "Catppuccin Latte"
 	else
 		if [ (uname) = "Darwin" ]
 			osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to true'
 		end
-		set -g theme_color_scheme zenburn
+		set -g theme_color_scheme "Catppuccin Mocha"
 		set -gx BAT_THEME "Catppuccin Mocha"
 	end
 end
