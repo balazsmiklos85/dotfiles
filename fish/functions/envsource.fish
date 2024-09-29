2ba897ca747b17f77e9218f6532717ea19919d32
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-09-29 22:22:15 +0200

    🎨 code formatting.

--- fish/functions/envsource.fish
+++ fish/functions/envsource.fish
@@ -1,6 +1,6 @@
 function envsource
     # Originally from: https://github.com/ngladkikh/fish_env/blob/master/load.fish
-set -l file (string trim $argv[1]) # Trim whitespace from the first argument
+    set -l file (string trim $argv[1]) # Trim whitespace from the first argument
     if test -z "$file" # Check if the file variable is empty
         set file .env # Default to ".env" if no argument is provided
     end
@@ -10,12 +10,12 @@ set -l file (string trim $argv[1]) # Trim whitespace from the first argument
     end
     while read -la line
         if not string match -q "#*" $line
-          set -l key (string trim (string split -m1 "=" $line)[1])
-          set -l value (string trim (string split -m1 "=" $line)[2..])
-          if test -n "$key"
-              set -xg $key (string join "=" $value | string trim)
-          end
+            set -l key (string trim (string split -m1 "=" $line)[1])
+            set -l value (string trim (string split -m1 "=" $line)[2..])
+            if test -n "$key"
+                set -xg $key (string join "=" $value | string trim)
+            end
         end
-    end < $file
+    end <$file
 
 end
