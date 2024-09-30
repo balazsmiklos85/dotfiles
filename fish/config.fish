e263120db5341ab574597cbec200245bf914a637
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-09-30 22:01:18 +0200

    🎨 code formatting.

--- fish/config.fish
+++ fish/config.fish
@@ -7,24 +7,24 @@ set -gx FZF_ALT_C_COMMAND "fd -t d . $HOME"
 set -gx PATH $HOME/.local/bin $PATH
 set -gx PATH $HOME/.sdkman $PATH
 
-if [ (uname) = "Darwin" ]
-	set -gx PATH /opt/homebrew/bin $PATH
-	set -gx PATH $HOME/DEV/projects/hybris/core-customize/hybris/bin/platform/apache-ant/bin $PATH
-	set -gx PATH $HOME/.nvm $PATH
+if [ (uname) = Darwin ]
+    set -gx PATH /opt/homebrew/bin $PATH
+    set -gx PATH $HOME/DEV/projects/hybris/core-customize/hybris/bin/platform/apache-ant/bin $PATH
+    set -gx PATH $HOME/.nvm $PATH
 else
     set -gx PATH $HOME/.rvm/gems/ruby-3.3.0/bin $PATH
 end
 
 set -gx MAILCAPS $HOME/.config/mailcap
 
-if command -v fzf > /dev/null
-	fzf --fish | source
+if command -v fzf >/dev/null
+    fzf --fish | source
 end
 
 zoxide init fish | source
 
-if command -v thefuck > /dev/null
-	thefuck --alias | source
+if command -v thefuck >/dev/null
+    thefuck --alias | source
 end
 
 alias cd='z'
@@ -33,4 +33,3 @@ alias tree='lsd --tree'
 alias cat='bat --style plain'
 
 source ~/.config/fish/secrets.fish
-
