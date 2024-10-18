25a223e8c5910285def5aeb39d9f939d0098dd57
Balazs Miklos <balazsmiklos85@gmail.com>
2024-10-18 13:10:14 +0200

    🧑‍💻 using the installed `gradle` command instead of the wrapper if available.

--- fish/functions/gw.fish
+++ fish/functions/gw.fish
@@ -1,12 +1,15 @@
 #!/usr/bin/fish
 
 function gw
-	if test -x "./gradlew"
+    if test -x gradle
         envsource .env
         nvm use
-		./gradlew $argv
-	else
-		echo "Gradle wrapper not found in the current directory."
-	end
+        gradle $argv
+    else if test -x "./gradlew"
+        envsource .env
+        nvm use
+        ./gradlew $argv
+    else
+        echo "Gradle wrapper not found in the current directory."
+    end
 end
-
