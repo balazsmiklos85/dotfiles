4a9d3be58a452d457d9d4444e63b94106da3b67f
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-10-21 08:49:30 +0200

    🧑‍💻 not calling `nvm` if not available.

--- fish/functions/gw.fish
+++ fish/functions/gw.fish
@@ -1,13 +1,17 @@
 #!/usr/bin/fish
 
 function gw
-    if test -x gradle
+    if command -v gradle >/dev/null
         envsource .env
-        nvm use
+        if command -v nvm >/dev/null
+            nvm use
+        end
         gradle $argv
     else if test -x "./gradlew"
         envsource .env
-        nvm use
+        if command -v nvm >/dev/null
+            nvm use
+        end
         ./gradlew $argv
     else
         echo "Gradle wrapper not found in the current directory."
