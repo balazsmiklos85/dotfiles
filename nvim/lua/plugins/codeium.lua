7eadefb00e80db2ca65b5061ff63e1a3a2ab1d0a
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-11-11 20:24:11 +0100

    🚚 condition organized into a function.

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -1,13 +1,13 @@
-local enabled_in_directories = { ".config", "book_club" }
-local current_directory = vim.fn.getcwd()
-local is_enabled = false
-for _, directory in ipairs(enabled_in_directories) do
-	if current_directory:find(directory, 1, true) then
-		is_enabled = true
-	end
-end
 return {
 	"Exafunction/codeium.vim",
 	event = "BufEnter",
-	cond = is_enabled,
+	cond = function()
+		local current_directory = vim.fn.getcwd()
+		for _, directory in ipairs({ ".config", "book_club" }) do
+			if current_directory:find(directory, 1, true) then
+				return true
+			end
+		end
+		return false
+	end,
 }
