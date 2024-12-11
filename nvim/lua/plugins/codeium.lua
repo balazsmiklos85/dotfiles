872588eb241f7ce1b68fd9021a704bbff8484158
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-12-11 11:24:43 +0100

    🔧 Enabling Codeium for my Project Euler solutions.

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -3,7 +3,7 @@ return {
 	event = "BufEnter",
 	cond = function()
 		local current_directory = vim.fn.getcwd()
-		for _, directory in ipairs({ ".config", "book_club", "semantify_table" }) do
+		for _, directory in ipairs({ ".config", "book_club", "project_euler", "semantify_table" }) do
 			if current_directory:find(directory, 1, true) then
 				return true
 			end
