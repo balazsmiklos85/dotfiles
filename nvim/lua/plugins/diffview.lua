eb4c62369e0e2ccc744d2123d3bec6b1665cfb2d
Miklós Balázs <balazsmiklos85+git@gmail.com>
2024-12-30 14:33:49 +0100

    🔧 Loading programming related plugins only for the specific languages and git related plugins only in git repos.

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,4 +1,7 @@
 return {
-  'sindrets/diffview.nvim'
+	"sindrets/diffview.nvim",
+	cond = function()
+		local result = vim.fn.system("git rev-parse --is-inside-work-tree 2>/dev/null")
+		return vim.trim(result) == "true"
+	end,
 }
-
