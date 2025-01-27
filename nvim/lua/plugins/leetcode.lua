6e48b0825a813a674c453d992fb84fd577af68a9
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-01-27 19:30:01 +0100

    ➕ `leetcode.nvim`

---
+++ nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,10 @@
+return {
+	"kawre/leetcode.nvim",
+	build = ":TSUpdate",
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+	},
+	opts = {},
+}
