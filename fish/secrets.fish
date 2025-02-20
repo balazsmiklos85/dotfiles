615f6f610a46061a305ba8f38931bcf0fa7dc5d8
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-02-20 10:33:06 +0100

    🐛 Only setting secrets if they are available.

--- fish/secrets.fish
+++ fish/secrets.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
 
-set -xg GEMINI_API_KEY (pass show token/gemini 2>/dev/null)
-set -xg SONARCLOUD_TOKEN (pass show token/sonarcloud 2>/dev/null)
-
+if command -v pass >/dev/null 
+   set -xg GEMINI_API_KEY (pass show token/gemini 2>/dev/null)
+   set -xg SONARCLOUD_TOKEN (pass show token/sonarcloud 2>/dev/null)
+end
