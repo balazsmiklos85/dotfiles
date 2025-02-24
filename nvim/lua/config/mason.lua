a1f4ad8149dc2daf16700922280a98f6cc4b1412
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-02-24 08:44:06 +0100

    🔧 conditionally ensuring LSPs.

--- nvim/lua/config/mason.lua
+++ nvim/lua/config/mason.lua
@@ -1,33 +1,44 @@
-local util = require("util.functions")
-
 local lsps_to_install = {
-	-- Java
-	"jdtls",
 	-- Lua
 	"lua_ls",
 }
 local tools_to_install = {
-	-- HTML, JavaScript, CSS, GraphQL, JSON, YAML, Markdown
-	"prettier",
-	-- Java
-	"google-java-format",
-	"java-debug-adapter",
-	"java-test",
 	-- Lua
 	"stylua",
 }
 
-if util.is_macos() or util.is_wsl() or util.is_vm() then
-	table.insert(lsps_to_install, "angularls")
-	table.insert(lsps_to_install, "terraformls")
-else
-	-- Ruby
-	table.insert(lsps_to_install, "rubocop")
-	table.insert(lsps_to_install, "ruby_lsp")
+if vim.fn.executable("cargo") == 1 then
 	-- Rust
 	table.insert(lsps_to_install, "rust_analyzer")
 	table.insert(tools_to_install, "codelldb")
 end
+
+if vim.fn.executable("npm") == 1 then
+	-- Angular
+	table.insert(lsps_to_install, "angularls")
+	-- HTML, JavaScript, CSS, GraphQL, JSON, YAML, Markdown
+	table.insert(tools_to_install, "prettier")
+end
+
+if vim.fn.executable("rvm") == 1 then
+	-- Ruby
+	table.insert(lsps_to_install, "rubocop")
+	table.insert(lsps_to_install, "ruby_lsp")
+end
+
+if vim.fn.executable("sdk") == 1 then
+	-- Java
+	table.insert(lsps_to_install, "jdtls")
+	table.insert(tools_to_install, "google-java-format")
+	table.insert(tools_to_install, "java-debug-adapter")
+	table.insert(tools_to_install, "java-test")
+end
+
+if vim.fn.executable("terraform") == 1 then
+	-- Terraform
+	table.insert(lsps_to_install, "terraformls")
+end
+
 require("mason").setup()
 require("mason-lspconfig").setup({
 	ensure_installed = lsps_to_install,
