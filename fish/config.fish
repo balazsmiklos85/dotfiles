30a92c0949f279cccac8644b02c6cca87fa698ff
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-04-26 09:18:58 +0200

    ➕ Trying `starship`.

--- fish/config.fish
+++ fish/config.fish
@@ -38,6 +38,10 @@ if command -v zoxide >/dev/null
     zoxide init fish | source
 end
 
+if command -v starship >/dev/null
+    starship init fish | source
+end
+
 if command -v thefuck >/dev/null
     thefuck --alias | source
 end
