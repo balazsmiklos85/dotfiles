c1047a5974897346ca5f0e1b37aa7e140bd84638
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-05-10 19:07:31 +0200

    🔧 showing rust-analyzer messages inline.

---
+++ nvim/ftplugin/rust.lua
@@ -0,0 +1,13 @@
+vim.diagnostic.config({
+	float = true,
+	jump = {
+		float = false,
+		wrap = true,
+	},
+	severity_sort = false,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	virtual_lines = false,
+	virtual_text = true,
+})
