c1a13f0122e6134a9701278a51fa60e18b33d464
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-05-18 08:56:21 +0200

    ⚡ caching the OS symbol for Starship.

--- fish/functions/print_os_symbol.fish
+++ fish/functions/print_os_symbol.fish
@@ -1,15 +1,18 @@
 function print_os_symbol
-    if is_wsl
-        echo "󰍲 "
-    else if test (uname) = Darwin
-        echo " "
-    else if test -f /etc/os-release; and grep -q openSUSE /etc/os-release
-        echo " "
-    else if test (uname) = Linux
-        echo " "
-    else if string match -q "*MINGW*" (uname -a); or string match -q "*MSYS*" (uname -a)
-        echo "󰍲 "
-    else
-        echo "? "
+    if not set -q OS_SYMBOL
+        if is_wsl
+            set -gx OS_SYMBOL "󰍲"
+        else if test (uname) = Darwin
+            set -gx OS_SYMBOL ""
+        else if test -f /etc/os-release; and rg -q openSUSE /etc/os-release
+            set -gx OS_SYMBOL ""
+        else if test (uname) = Linux
+            set -gx OS_SYMBOL ""
+        else if string match -q "*MINGW*" (uname -a); or string match -q "*MSYS*" (uname -a)
+            set -gx OS_SYMBOL "󰍲"
+        else
+            set -gx OS_SYMBOL "?"
+        end
+        echo $OS_SYMBOL
     end
 end
