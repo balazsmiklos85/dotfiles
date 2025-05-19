467f4f86c92e637f06d1b9a32955a3937d1c3b8e
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-05-19 08:51:51 +0200

    🐛 setting the environment variable universally.

--- fish/functions/print_os_symbol.fish
+++ fish/functions/print_os_symbol.fish
@@ -1,17 +1,17 @@
 function print_os_symbol
     if not set -q OS_SYMBOL
         if is_wsl
-            set -gx OS_SYMBOL "󰍲"
+            set -U OS_SYMBOL "󰍲"
         else if test (uname) = Darwin
-            set -gx OS_SYMBOL ""
+            set -U OS_SYMBOL ""
         else if test -f /etc/os-release; and rg -q openSUSE /etc/os-release
-            set -gx OS_SYMBOL ""
+            set -U OS_SYMBOL ""
         else if test (uname) = Linux
-            set -gx OS_SYMBOL ""
+            set -U OS_SYMBOL ""
         else if string match -q "*MINGW*" (uname -a); or string match -q "*MSYS*" (uname -a)
-            set -gx OS_SYMBOL "󰍲"
+            set -U OS_SYMBOL "󰍲"
         else
-            set -gx OS_SYMBOL "?"
+            set -U OS_SYMBOL "?"
         end
     end
     echo $OS_SYMBOL
