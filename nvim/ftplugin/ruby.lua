7e22735fda27afa374b40e93371b6a72a9b73449
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-06-17 11:49:07 +0200

    🔧 showing LSP warnings/errors for Ruby as well.

--- nvim/ftplugin/ruby.lua
+++ nvim/ftplugin/ruby.lua
@@ -22,3 +22,17 @@ lspconfig.ruby_lsp.setup({
 })
 
 require("dap-ruby").setup()
+
+vim.diagnostic.config({
+	float = true,
+	jump = {
+		float = false,
+		wrap = true,
+	},
+	severity_sort = false,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	virtual_lines = false,
+	virtual_text = true,
+})
