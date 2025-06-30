12d1d8ee65c571a7ea1599ec8b876f3fe2fa7441
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-06-30 07:14:05 +0000

    🔧 setting the `nvim-lspconfig` version conditionally in old versions.

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,4 +1,10 @@
+local lsp_version = nil
+if vim.version().minor < 10 then
+  lsp_version = 'v1.8.0'
+end
+
 return {
-  'neovim/nvim-lspconfig'
+  'neovim/nvim-lspconfig',
+  version = lsp_version,  
 }
 
