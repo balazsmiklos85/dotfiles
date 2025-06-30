a54937a9ce632ea41982435fbfc9c1f0199e8d1f
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-06-30 07:22:44 +0000

    🔧 counting LSP clients the old way in older versions.

--- nvim/lua/config/lsp_start.lua
+++ nvim/lua/config/lsp_start.lua
@@ -1,7 +1,13 @@
 vim.api.nvim_create_autocmd("BufReadPost", {
 	pattern = "*",
 	callback = function()
-		if #vim.lsp.get_clients() == 0 then
+		local client_count = -1
+		if vim.version().minor < 10 then
+			client_count = #vim.lsp.get_active_clients()
+		else
+			client_count = #vim.lsp.get_clients()
+		end
+		if client_count == 0 then
 			vim.cmd("LspStart")
 		end
 	end,
