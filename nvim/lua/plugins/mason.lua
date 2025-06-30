29c11422a45c9990cd0dcb7cbb130fbe8cfbf335
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-06-30 07:05:12 +0000

    🔧 setting the `mason` and `mason-lsp` versions conditionally in old versions.

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,9 +1,18 @@
+local mason_version = nil
+local mason_lsp_version = nil
+if vim.version().minor < 10 then
+  mason_version = 'v1.11.0'
+  mason_lsp_version = 'v1.32.0'
+end
+
 return {
   {
     'williamboman/mason.nvim',
+    version = mason_version,
   },
   {
     'williamboman/mason-lspconfig.nvim',
+	version = mason_lsp_version,
     requires = {
       'neovim/nvim-lspconfig',
       'williamboman/mason.nvim',
