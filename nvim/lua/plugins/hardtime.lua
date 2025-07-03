6825066a3606c8869d2713ac0c12dfd689af9c44
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-07-03 11:33:41 +0200

    🔧 disabled more navigation keys in `hardtime.nvim`.

--- nvim/lua/plugins/hardtime.lua
+++ nvim/lua/plugins/hardtime.lua
@@ -8,5 +8,22 @@ return {
 		"MunifTanjim/nui.nvim",
 		"nvim-lua/plenary.nvim"
 	},
-	opts = {}
+	opts = {
+		disabled_keys = {
+			["<Up>"] = { "n", "i", "v" },
+			["<Down>"] = { "n", "i", "v" },
+			["<Left>"] = { "n", "i", "v" },
+			["<Right>"] = { "n", "i", "v" },
+			["<PageUp>"] = { "n", "i", "v" },
+			["<PageDown>"] = { "n", "i", "v" },
+			["<Home>"] = { "n", "i", "v" },
+			["<End>"] = { "n", "i", "v" },
+			["<S-Home>"] = { "n", "i", "v" },
+			["<S-End>"] = { "n", "i", "v" },
+			["<S-Up>"] = { "n", "i", "v" },
+			["<S-Down>"] = { "n", "i", "v" },
+			["<S-Left>"] = { "n", "i", "v" },
+			["<S-Right>"] = { "n", "i", "v" },
+		},
+	}
 }
