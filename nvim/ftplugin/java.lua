3f04d679f61335e22741496cb74bb0216df53045
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-07-06 19:56:46 +0200

    🧑‍💻 showing the LSP's problems inline in the case of Java too.

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -40,3 +40,17 @@ vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.expandtab = true
 
+vim.diagnostic.config({
+	float = true,
+	jump = {
+		float = false,
+		wrap = true,
+	},
+	severity_sort = false,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	virtual_lines = false,
+	virtual_text = true,
+})
+
