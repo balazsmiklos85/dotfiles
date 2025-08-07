ae6872cc308c3d0b19642d8528cac2a07f9c41cc
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-08-07 17:50:14 +0200

    🔧 enabling telemetry for Zed.

--- fish/secrets.fish
+++ fish/secrets.fish
@@ -2,5 +2,6 @@
 
 if command -v pass >/dev/null
     set -xg GEMINI_API_KEY (pass show token/gemini 2>/dev/null)
+    set -xg LASTFM_PASSWORD (pass show music/last_fm 2>/dev/null)
     set -xg SONARCLOUD_TOKEN (pass show token/sonarcloud 2>/dev/null)
 end
