fa874aee540ccd798dde4449547ab4722d0676b6
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-08-13 10:37:42 +0200

    🧑‍💻 CLI alias for Zed.

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -6,5 +6,9 @@ if command -v bat >/dev/null
     alias cat="bat --style plain"
 end
 
+if command -v flatpak >/dev/null
+    alias zed 'flatpak run dev.zed.Zed'
+end
+
 alias :w 'echo "Dammit Jim, I\'m a fish, not a vi!"; test 0 -eq 1'
 alias :wq 'echo "Dammit Jim, I\'m a fish, not a vi!"; test 0 -eq 1'
