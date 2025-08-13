c0480c677a8686cc63c11accebbb1d12a97d7368
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-08-13 10:24:09 +0200

    ♻️aliases moved to their own file.

---
+++ fish/aliases.fish
@@ -0,0 +1,10 @@
+if command -v thefuck >/dev/null
+    thefuck --alias | source
+end
+
+if command -v bat >/dev/null
+    alias cat="bat --style plain"
+end
+
+alias :w 'echo "Dammit Jim, I\'m a fish, not a vi!"; test 0 -eq 1'
+alias :wq 'echo "Dammit Jim, I\'m a fish, not a vi!"; test 0 -eq 1'
