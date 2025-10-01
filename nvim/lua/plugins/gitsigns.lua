3b10e2d2a80694436f552352774298971b373652
Miklós Balázs <balazsmiklos85+git@gmail.com>
2025-10-01 19:37:22 +0200

    🔧 Always using the Git related plugins when we are in a container.

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,11 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	cond = function()
+		local container_file = io.open("/run/.containerenv", "r")
+		if container_file then
+			container_file:close()
+			return true
+		end
 		local result = vim.fn.system("git rev-parse --is-inside-work-tree 2>/dev/null")
 		return string.find(result, "true")
 	end,
