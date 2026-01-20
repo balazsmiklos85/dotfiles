5d81ac10ab7c3b436480c9280d86d777ba050048
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-01-20 15:08:15 +0100

    🧑💻 Timewarrior aliases.

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -10,5 +10,11 @@ if command -v flatpak >/dev/null
     alias zed 'flatpak run dev.zed.Zed'
 end
 
+if command -v timew >/dev/null
+    alias start_work 'timew start work'
+    alias stop_work 'timew stop work'
+    alias balance 'timew balance :year'
+end
+
 alias :w 'echo "Dammit Jim, I\'m a fish, not a vi!"; test 0 -eq 1'
 alias :wq 'echo "Dammit Jim, I\'m a fish, not a vi!"; test 0 -eq 1'
