31e0163cb5cb6f969052fd0fe76a5bf8d3b59808
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-01-21 09:06:02 +0100

    🔧 configuring `pylsp` for Neovim 0.11+.

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -1,1 +1,11 @@
-require("lspconfig").pylsp.setup({})
+vim.lsp.config.pylsp = {
+  cmd = { "pylsp" },
+  filetypes = { "python" }
+}
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "python",
+	callback = function(args)
+		vim.lsp.enable("pylsp", { bufnr = args.buf })
+	end,
+})
