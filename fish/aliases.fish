d30ab71e8e37bb2bf925da79f4b29fe4fa4922e3
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-02-04 08:58:12 +0100

    🧑‍💻 aliases for Gradle compilation/test with minimized outputs.

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -15,6 +15,9 @@ if command -v gh >/dev/null
     alias watch_checks 'gh pr checks --watch'
 end
 
+alias gw_compile 'gw compileJava compileTestJava --console=plain 2>&1 | rg -A 4 -B 1 \'java:\d+: error\''
+alias gw_test 'gw clean test --console=plain 2>&1 | rg -v \'> Task|BUILD FAILED\' | rg \'FAILED|See the report at\''
+
 if command -v timew >/dev/null
     alias start_work 'timew start work'
     alias stop_work 'timew stop work'
