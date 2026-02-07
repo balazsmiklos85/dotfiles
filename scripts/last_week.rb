9af484637fa4b6752596780739c96e4ffda0a0a8
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-02-07 08:00:10 +0100

    🚨 Style/StringLiterals: Prefer single-quoted strings when you don't need string interpolation or special symbols.

--- scripts/last_week.rb
+++ scripts/last_week.rb
@@ -7,7 +7,7 @@ monday_date = Date.today - (Date.today.wday - 1) % 7
 command = "git log main --author='#{author}' --since='#{monday_date} 00:00:00' --pretty=format:%s"
 commits = `#{command}`
 if commits.empty?
-  puts "No commits found since Monday."
+  puts 'No commits found since Monday.'
 else
   puts commits
 end
