46c7a6ecc9c42adaf62858f6569f852e192a5765
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-02-12 21:51:05 +0100

    🚨 `rubocop -A`

--- scripts/github_output.rb
+++ scripts/github_output.rb
@@ -1,15 +1,20 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'English'
 require 'json'
 
 def fetch_json(cmd)
   output = `#{cmd}`
-  abort "Error running command: #{cmd}\nEnsure you are in a git repository with an active PR." unless $?.success?
+  unless $CHILD_STATUS.success?
+    abort "Error running command: #{cmd}\nEnsure you are in a git repository with an active PR."
+  end
 
   JSON.parse(output)
 end
 
 branch_name = `git rev-parse --abbrev-ref HEAD`.strip
-abort "Could not detect branch name. Are you in a git repository?" if branch_name.empty?
+abort 'Could not detect branch name. Are you in a git repository?' if branch_name.empty?
 
 runs = fetch_json "gh run list --branch '#{branch_name}' --status failure --limit 1 --json databaseId"
 abort "No failed runs found for branch '#{branch_name}'." if runs.empty?
@@ -17,10 +22,10 @@ abort "No failed runs found for branch '#{branch_name}'." if runs.empty?
 run_id = runs[0]['databaseId']
 run_data = fetch_json "gh run view #{run_id} --json jobs"
 failed_jobs = run_data['jobs'].select { |j| j['conclusion'] == 'failure' }
-abort "No specific failing jobs were found." if failed_jobs.empty?
+abort 'No specific failing jobs were found.' if failed_jobs.empty?
 
 failed_jobs.each do |job|
-  IO.popen("gh run view --job #{job['databaseId']} --log", "r") do |io|
+  IO.popen("gh run view --job #{job['databaseId']} --log", 'r') do |io|
     io.each_line do |line|
       print line
     end
