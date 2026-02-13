0d829c1fbf9e9e298e04685e1b90254d9f531e2b
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-02-13 13:26:02 +0100

    🧑‍💻 adding a right margin.

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,6 +6,8 @@ vim.cmd([[set conceallevel=1]])
 vim.cmd([[set number]])
 vim.cmd([[set list]])
 vim.cmd([[autocmd FileType mail setlocal textwidth=0]])
+vim.opt_local.colorcolumn = "120"
+vim.opt_local.textwidth = 120
 
 require("config.lazy")
 require("config.mason")
