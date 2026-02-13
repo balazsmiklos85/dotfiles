5a7e543fec7df2b19e2d9d5ce13b05bdddbcb7d1
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-02-13 14:23:17 +0100

    🔧 right margin at 150 for Java

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -39,6 +39,8 @@ require('dap').configurations.java = {
 vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.expandtab = true
+vim.opt_local.colorcolumn = "120"
+vim.opt_local.textwidth = 120
 
 vim.diagnostic.config({
 	float = true,
