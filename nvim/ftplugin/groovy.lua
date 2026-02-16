0812a0b5ae38423ec0f55d464273570e5297a956
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-02-16 08:46:01 +0100

    🔧 using the new LSP config for Groovy

--- nvim/ftplugin/groovy.lua
+++ nvim/ftplugin/groovy.lua
@@ -1,7 +1,9 @@
-local lspconfig = require('lspconfig')
-lspconfig.groovyls.setup {
-  cmd = {
-    "java", "-jar", os.getenv("HOME") .. "/.local/bin/groovy-language-server/build/libs/groovy-language-server-all.jar"
-  }
-}
+vim.lsp.config("groovyls", {
+	cmd = {
+		"java",
+		"-jar",
+		os.getenv("HOME") .. "/.local/bin/groovy-language-server/build/libs/groovy-language-server-all.jar",
+	},
+})
 
+vim.lsp.enable("groovyls")
