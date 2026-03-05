4a9d7b1ebf2ef6eeb588ac9026d106e5d78ac6b5
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-03-05 12:38:38 +0100

    🧑‍💻 killing Docker containers should do just exactly that.

--- fish/functions/kill_docker.fish
+++ fish/functions/kill_docker.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/fish
 
 function kill_docker
-    echo '> docker ps -q | xargs -n 1 -r docker kill && docker container prune -f'
-    docker ps -q | xargs -n 1 -r docker kill && docker container prune -f
+    echo '> docker ps -q | xargs -n 1 -r docker kill'
+    docker ps -q | xargs -n 1 -r docker kill
 end
