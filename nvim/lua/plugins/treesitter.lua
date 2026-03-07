77204eea88f40e6c32ce65d41f14d9e0ae71be7b
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-03-07 09:48:43 +0100

    🐛 Treesitter is set up properly at Neovim start.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,38 +1,41 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	opts = {
-		ensure_installed = {
-			"csv",
-			"dockerfile",
-			"fish",
-			"gitcommit",
-			"gitignore",
-			"groovy",
-			"html",
-			"java",
-			"javascript",
-			"lua",
-			"ruby",
-			"rust",
-			"sql",
-			"terraform",
-			"typescript",
-			"vim",
-			"xml",
-		},
-		sync_install = false,
-		auto_install = true,
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"csv",
+				"dockerfile",
+				"fish",
+				"gitcommit",
+				"gitignore",
+				"groovy",
+				"html",
+				"java",
+				"javascript",
+				"lua",
+				"ruby",
+				"rust",
+				"sql",
+				"terraform",
+				"typescript",
+				"vim",
+				"xml",
+			},
+			sync_install = false,
+			auto_install = true,
 
-		highlight = {
-			enable = true,
+			highlight = {
+				enable = true,
 
-			disable = function(lang, buf)
-				local max_filesize = 200 * 1024 -- 100 KB
-				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-				if ok and stats and stats.size > max_filesize then
-					return true
-				end
-			end,
-		},
-	},
+				disable = function(lang, buf)
+					local max_filesize = 200 * 1024 -- 200 KB
+					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+				end,
+			},
+		})
+	end,
 }
+
