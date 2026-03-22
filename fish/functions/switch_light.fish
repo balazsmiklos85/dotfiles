3122e8e86d923bdeab52f06c8b0f5a306b78d51d
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-03-22 10:57:53 +0100

    🐛 not calling Alacritty if it is not installed.

--- fish/functions/switch_light.fish
+++ fish/functions/switch_light.fish
@@ -3,7 +3,9 @@ function switch_light
 
     if test $status -eq 0
         osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to false'
-        alacritty msg config "$(cat ~/.config/alacritty/catppuccin-latte.toml)"
+        if command -v alacritty >/dev/null
+            alacritty msg config "$(cat ~/.config/alacritty/catppuccin-latte.toml)"
+        end
         alias delta "delta --light"
         fish_config theme choose "Catppuccin Mocha" --color-theme=light
         set -gx BAT_THEME "Catppuccin Latte"
@@ -13,7 +15,9 @@ function switch_light
         if [ (uname) = Darwin ]
             osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to true'
         end
-        alacritty msg config "$(cat ~/.config/alacritty/catppuccin-mocha.toml)"
+        if command -v alacritty >/dev/null
+            alacritty msg config "$(cat ~/.config/alacritty/catppuccin-mocha.toml)"
+        end
         alias delta "delta --dark"
         fish_config theme choose "Catppuccin Mocha" --color-theme=dark
         set -gx BAT_THEME "Catppuccin Mocha"
