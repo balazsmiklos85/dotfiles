17772df44c54a56a8ed1ab0a3deeb829f7aba948
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-04-15 10:50:58 +0200

    🔥 manual hack to start the LSP is no longer needed. Also no longer supported.

--- nvim/lua/config/lsp_start.lua
+++ nvim/lua/config/lsp_start.lua
@@ -38,17 +38,3 @@ vim.lsp.config("ruby_lsp", {
 })
 vim.lsp.enable("ruby_lsp")
 
-vim.api.nvim_create_autocmd("BufReadPost", {
-	pattern = "*",
-	callback = function()
-		local client_count = -1
-		if vim.version().minor < 10 then
-			client_count = #vim.lsp.get_active_clients()
-		else
-			client_count = #vim.lsp.get_clients()
-		end
-		if client_count == 0 then
-			vim.cmd("LspStart")
-		end
-	end,
-})
