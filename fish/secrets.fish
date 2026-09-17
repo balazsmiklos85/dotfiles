32831383a879ee8f41800348fd7bc3c49499054f
Miklós Balázs <balazsmiklos85+git@gmail.com>
2026-09-17 08:15:34 +0200

    Add LLM gateway environment variable

--- fish/secrets.fish
+++ fish/secrets.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
 
 if command -v pass >/dev/null
+    set -xg OPENCODE_LLM_GATEWAY_TEAM (pass show opencode_llm_gateway_team 2>/dev/null)
     set -xg SESSION_SECRET (pass show book_club/session_secret 2>/dev/null)
 end
